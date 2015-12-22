60459bebc8df335b40e7995a0eee988a5d6835d0
Justin Bellomi <sumason2@gmail.com>
2015-12-22 10:32:05 -0800

    Trying to get the code ready to be highlighted. Refactored some code into functions so it reads a little cleaner.
    * web/lib/main.js:
        - Pulled the code out that generates the summary table into its own
          function for readability.
    
        - Pulled the code that counts the tags from the HTML into its own function
          for readability.
    
        - During the string replace, added spans that will be used for
          highlighting tags.
    
        - Removed 'find.' from the string replace because we will eventually need
          to strip this out and it doesn't give us any extra functionality yet.
    
    * web/stylesheets/main.css:
        - Added CSS for the code section.

--- web/lib/main.js
+++ web/lib/main.js
@@ -1,3 +1,35 @@
+function generateSummaryTable(tags) {
+    // This should probably be replaced with something like Handlebars.
+    var summary = '<table><tr><th>Tag</th><th>Appearances</th></tr>';
+    for (var key in tags) {
+        summary = summary + '<tr><td>' +
+            key       + ':</td><td>' +
+            tags[key] + '</td></tr>';
+    }
+    summary = summary + '</table>'; 
+    return summary;
+}
+
+function parseTagsFromHTML(string) {
+    // Iterate over tags
+    var tags = {};
+    $(string).find("div").each(function(index, element) {
+        var tagName = element.className.toLowerCase();
+
+        // Ignore Comments.
+        if (tagName.slice(0,1) != "!") {
+            // Initialize value at 0 if it doesn't exist.
+            if (!(tagName in tags)) {
+                tags[tagName] = 0;
+            }
+            // Tally tagName.
+            tags[tagName]++;
+        }
+    });
+
+    return tags;
+}
+
 function fetchURL() {
     // grab URL from input.
     var url = $("#url-input").val();
@@ -6,31 +38,15 @@ function fetchURL() {
     $.get("/fetchURL.php?url=" + url, function(data,status) {
         // Replace tags with div tags.
         var string = data.replace(
-            /<([^>\/ ]+)/g,
-            "<div class=\"find.$1\"> &lt;$1"
+            /<([^>\/ ]+)([^>]*)>/g,
+            "<div class=\"$1\"><span class=\"highlight.$1\">&lt;$1$2&gt;</span>"
         ).replace(
             /<\/([^> ]+)>/g,
-            "&lt;/$1&gt;</div>"
+            "<span class=\"highlight.$1\">&lt;/$1&gt;</span></div>"
         );
 
-        // Iterate over tags
-        var tags = {};
-        $(string).find("div").each(function(index, element) {
-            var tagName = element.className.toLowerCase();
-            if (!(tagName in tags)) {
-                tags[tagName] = 0;
-            }
-            tags[tagName]++;
-        });
-
-        // This should probably be replaced with something like Handlebars.
-        var summary = '<table><tr><th>Tag</th><th>Appearances</th></tr>';
-        for (var key in tags) {
-            summary = summary + '<tr><td>' +
-                key       + ':</td><td>' +
-                tags[key] + '</td></tr>';
-        }
-        summary = summary + '</table>'; 
+        var tags    = parseTagsFromHTML(string);
+        var summary = generateSummaryTable(tags);
         $("#summary").html(summary);
         $("#html-results").html(string);
     });
