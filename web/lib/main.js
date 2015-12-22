548218146d6816ad184197a1aaa7f1cef44da110
Justin Bellomi <sumason2@gmail.com>
2015-12-22 00:17:40 -0800

    Recursive approach was not working trying another method of iterating through elements using jQuery.
    * web/lib/main.js:
        - Removed recursive function.
        - added another jQuery method for iterating over elements.

--- web/lib/main.js
+++ web/lib/main.js
@@ -1,23 +1,20 @@
-function findTags(node, tags) {
-    node.each(function() {
-        findTags(this, tags);
-    });
-    var tagName = node.nodeName.toLowerCase();
-    if (!(tagName in tags)) {
-        tags[tagName] = 0;
-    }
-    tags[tagName]++;
-}
-
 function fetchURL() {
     // grab URL from input.
     var url = $("#url-input").val();
 
     // get HTML response from URL
     $.get("/fetchURL.php?url=" + url, function(data,status) {
+        var html = $.parseHTML(data);
+
         // Iterate over tags
         var tags = {};
-        findTags($(data), tags);
+        $.each( html, function(index, element) {
+            var tagName = element.nodeName.toLowerCase();
+            if (!(tagName in tags)) {
+                tags[tagName] = 0;
+            }
+            tags[tagName]++;
+        });
 
         // This should probably be replaced with something like Handlebars.
         var summary = '<table><tr><th>Tag</th><th>Appearances</th></tr>';
