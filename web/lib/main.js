4191a70a7fcbfa46cbd9ebe1b99e0e0d6b1fe4db
Justin Bellomi <sumason2@gmail.com>
2015-12-21 20:58:32 -0800

    Testing ajax subroutine to fetch HTML from a URL.
    * web/lib/main.js:
        - Added jQuery to pull URL from input.
        - Added ajax call to pull data from a URL. Page should alert with results.
    
    * web/views/controls.html:
        - Added id attribute to input tag to easily pull using jQuery.

--- web/lib/main.js
+++ web/lib/main.js
@@ -1,4 +1,9 @@
 function fetchURL() {
+    // grab URL from input.
+    var url = $("#url-input").val();
+
+    // get HTML response from URL
+    $.ajax({ url: url, success: function(data) { alert(data); } });
     $("#html-results").html("Place URL Results here...");
     return false;
 }
